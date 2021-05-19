fb9888e1a35b5a9459800c9b91125d17e991896a
davidallenbaer <davidallenbaer@gmail.com>
2021-05-18 22:15:11 -0400

    Add project files.

---
+++ DesignPattern_FluentInterface/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPattern_FluentInterface
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FluentEmployee obj = new FluentEmployee();
+            
+            obj.NameOfTheEmployee("Dave Baer")
+                    .Born("06/01/1989")
+                    .WorkingOn("IT")
+                    .StaysAt("101 Easy Street");
+
+            Console.Read();
+        }
+    }
+}
